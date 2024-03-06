5312068d5b567970a76f44a17d48acd06171886f
bavi2477 <bavithra.sjh@gmail.com>
2024-03-06 14:20:43 +0530

    second commit

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 import express from 'express';
 import fs from 'fs';
+import {format} from 'date-fns';
 
 const app = express();
 
 const PORT = 4000;
 
 
-//getcall 
-app.get('/', (req,res) => {
-    res.status(200).json({message: "Hai my dear mentor Suresh, we will work to become a FSD developer"})
-}) 
+app.get('/write', (req,res) => {
+    let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
+    console.log("today",today);
+    const filePath = `TimeStamp/${today}.txt`;
+    fs.writeFileSync(filePath, `${today}` , 'utf8');
+    res.status(200).json({message: "File created successfully"})
+})
 
-app.get('/get-data', (req,res) => {
-    res.status(200).json({message: "data", data:{name:"Bavithra seenivasan"}})
-}) 
+app.get('/read', (req, res) => {
+    let files = fs.readdirSync('TimeStamp');
 
-app.get('/write-read', (req,res) => {
-    let today = fo
-    res.status(200).json()
+    const latestFile = files[files.length - 1];
+    const filePath = `TimeStamp/${latestFile}`;
+
+    let data = fs.readFileSync(filePath, 'utf8');
+    res.status(200).send(data);
 })
 
 //running 
 app.listen(PORT, () => {
-    console.log(`App i running in the port = ${PORT}`);
+    console.log(`App is running in the port = ${PORT}`);
 }) 
