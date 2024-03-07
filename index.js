5372c4d32f69646abc199197226d24bc39b44667
bavi2477 <bavithra.sjh@gmail.com>
2024-03-07 12:12:34 +0530

    new time

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 import express from 'express';
 import fs from 'fs';
-import {format} from 'date-fns';
+import { format } from 'date-fns';
 
 const app = express();
 
 const PORT = 4000;
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: `
+    res.status(200).json({
+        message: `<div style="text-align: center; padding-top:50px;">
     <p>Welcome to view the current timestamp.</p>
     <p>Add /write to view the success message.</p>
     <p>Add /read to read the current time.</p>
-`})
+    </div>`})
 })
 
 
-app.get('/write', (req,res) => {
+app.get('/write', (req, res) => {
     let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
-    console.log("today",today);
+    console.log("today", today);
     const filePath = `TimeStamp/${today}.txt`;
-    fs.writeFileSync(filePath, `${today}` , 'utf8');
-    res.status(200).json({message: "File created successfully"})
+    fs.writeFileSync(filePath, `${today}`, 'utf8');
+    res.status(200).json({ message: "File created successfully" })
 })
 
 app.get('/read', (req, res) => {
